9ae55fdd2908ad4c681ee2a713efb6ebf7c500b0
aaddsss111 <66762282+aaddsss111@users.noreply.github.com>
2024-10-09 14:24:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,12 +8,14 @@ let userID = '90cd4a77-141a-43c9-991b-08263cfe9c10';
 
 let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org'
 
-let subs = 'alvless.comorg.us.kg', 'VLESS.fxxk.dedyn.io';
+let primarySub = 'alvless.comorg.us.kg'; // 主选
+let backupSub = 'VLESS.fxxk.dedyn.io';   // 备选
 
-// 遍历处理或使用其中某一个
-subs.forEach(sub => {
-    console.log(sub); // 输出每个域名
-});
+// 根据某些条件选择其中一个域名
+let sub = someCondition ? primarySub : backupSub;
+
+console.log(sub); // 输出满足条件的域名
+
 
 let subconverter = 'SUBAPI.fxxk.dedyn.io';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假uuid和host订阅。
 let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
