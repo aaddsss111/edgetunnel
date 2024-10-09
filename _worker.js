83eef6afb6175a732eb085b5d390ed81f95986ea
aaddsss111 <66762282+aaddsss111@users.noreply.github.com>
2024-10-09 14:23:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ let userID = '90cd4a77-141a-43c9-991b-08263cfe9c10';
 
 let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org'
 
-let subs = ['alvless.comorg.us.kg', 'VLESS.fxxk.dedyn.io'];
+let subs = 'alvless.comorg.us.kg', 'VLESS.fxxk.dedyn.io';
 
 // 遍历处理或使用其中某一个
 subs.forEach(sub => {
